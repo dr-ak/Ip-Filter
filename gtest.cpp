3c41b86ab2296083c7d4a07572f71b1287375404
dr-ak <alex-r-1982@mail.ru>
2019-01-16 19:31:50 +0700

    testing gtest

--- gtest.cpp
+++ gtest.cpp
@@ -1,22 +1,19 @@
 #include "lib_ip_filter.hpp"
 #include <gtest/gtest.h>
-#include <string>
 
 
-/*TEST(GTest, TestSplit){
+TEST(GTest, TestSplit){
     std::vector <std::string> a = {"222", "173", "235", "246"};
     std::string str = "222.173.235.246";
     std::vector <std::string> b = split(str, '.');
 
     for (unsigned i = 0; i < a.size(); ++i)
         ASSERT_STREQ(a[i].c_str(), b[i].c_str());
-}*/
+}
 
 TEST(GTest, TestAllowPrintIp){
-    //std::string str = "222.173.235.246";
-    //ASSERT_TRUE(allow_print_ip("\\.173\\.", str));
-    ASSERT_TRUE(true);
-
+    std::string str = "222.173.235.246";
+    ASSERT_TRUE(allow_print_ip("\\.173\\.", str));
 }
 
 int main(int argc, char **argv)
