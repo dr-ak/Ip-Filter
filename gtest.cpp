1db41e7354438c54fa5c5ab3f1a104b0fbe47099
dr-ak <alex-r-1982@mail.ru>
2019-01-17 15:07:34 +0700

    Optimization

--- gtest.cpp
+++ gtest.cpp
@@ -1,19 +1,29 @@
 #include "lib_ip_filter.hpp"
 #include <gtest/gtest.h>
 
-
 TEST(GTest, TestSplit){
-    std::vector <std::string> a = {"222", "173", "235", "246"};
-    std::string str = "222.173.235.246";
-    std::vector <std::string> b = split(str, '.');
-
-    for (unsigned i = 0; i < a.size(); ++i)
-        ASSERT_STREQ(a[i].c_str(), b[i].c_str());
+    std::string str = "185.69.186.168	1	3";
+    std::string a = "185.69.186.168";
+    std::vector <std::string> b = split(str, '\t');
+    ASSERT_STREQ(a.c_str(), b[0].c_str());
+    str = "110.152.103.161	0	1";
+    a = "110.152.103.161";
+    b = split(str, '\t');
+    ASSERT_STREQ(a.c_str(), b[0].c_str());
+    str = "185.182.81.131	1	1";
+    a = "185.182.81.131";
+    b = split(str, '\t');
+    ASSERT_STREQ(a.c_str(), b[0].c_str());
 }
 
-TEST(GTest, TestAllowPrintIp){
+
+TEST(GTest, TestSplitToInt){
+    std::vector <int> a = {222, 173, 235, 246};
     std::string str = "222.173.235.246";
-    ASSERT_TRUE(allow_print_ip("\\.173\\.", str));
+    std::vector <int> b = split_to_int(str, '.');
+
+    for (unsigned i = 0; i < a.size(); ++i)
+        ASSERT_EQ(a[i], b[i]);
 }
 
 int main(int argc, char **argv)
--- lib_ip_filter.cpp
+++ lib_ip_filter.cpp
@@ -5,14 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <stdlib.h> 
-#include <regex>
 
-// ("",  '.') -> [""]
-// ("11", '.') -> ["11"]
-// ("..", '.') -> ["", "", ""]
-// ("11.", '.') -> ["11", ""]
-// (".11", '.') -> ["", "11"]
-// ("11.22", '.') -> ["11", "22"]
 std::vector<std::string> split(const std::string &str, char d)
 {
     std::vector<std::string> r;
@@ -32,28 +25,59 @@ std::vector<std::string> split(const std::string &str, char d)
     return r;
 }
 
-bool allow_print_ip(std::string rgxp, std::string ip)
+std::vector<int> split_to_int(const std::string &str, char d)
 {
-    std::regex regular(rgxp);
-    return std::regex_search(ip.c_str(), regular);
+    std::vector<int> r;
+
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while(stop != std::string::npos)
+    {
+        r.push_back(atoi(str.substr(start, stop - start).c_str()));
+
+        start = stop + 1;
+        stop = str.find_first_of(d, start);
+    }
+
+    r.push_back(atoi(str.substr(start).c_str()));
+
+    return r;
 }
 
-void print_ip_pool(const std::vector<std::vector<std::string>>& ip_pool, std::string rgxp = "")
+void print_ip(const std::vector<int>& ip)
 {
-    bool start;
-    std::string out;
-    for (auto& ip: ip_pool){
-        start = false;
-        out = "";
-        for (auto& ip_part: ip){
-            if (start){
-                out += ".";
-            } else {
-                start = true;
-            }
-            out += ip_part;
+    bool start = false;
+    for (auto& ip_part: ip){
+        if (start){
+            std::cout << ".";
+        } else {
+            start = true;
         }
-        if (allow_print_ip(rgxp, out))
-            std::cout << out << std::endl;
+            std::cout << ip_part;
     }
+    std::cout << std::endl;
+}
+
+void filter(const std::vector<std::vector<int>>& ip_pool, int  a = -1, int b = -1, int c = -1, int d = -1)
+{
+    unsigned i = ip_pool.size();
+    std::vector<int> param = {a, b, c, d};
+    for (int j = 0; j < 4; ++j){
+        if (param[j] > -1) while (ip_pool[i-1][j] <= param[j] && i > 0) --i;
+    }
+    if (a == -1 && b == -1 && c == -1 && d == -1) i = 0;
+        while (i < ip_pool.size()){
+            if((a == ip_pool[i][0] || a < 0) && (b == ip_pool[i][1] || b < 0)
+            && (c == ip_pool[i][2] || c < 0) && (d == ip_pool[i][d] || d < 0))
+                print_ip(ip_pool[i]);
+            ++i;
+        }
+    
+}
+
+void filter_any(const std::vector<std::vector<int>>& ip_pool, int  a)
+{
+    for (const auto& ip: ip_pool)
+        if (ip[0] == a || ip[1] == a || ip[2] == a || ip[3] == a)
+            print_ip(ip);
 }
